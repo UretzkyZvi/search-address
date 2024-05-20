1e1e8c4009594d3c36dd0ffbb2dd061e767910bd
greguretzky <uretsky.zvi@gmail.com>
2024-05-20 13:25:27 +0200

    chore: Disable TypeScript linting rules for unsafe assignments, member access, explicit any, and floating promises

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,10 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
